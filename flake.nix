840902910aee4ac3e4b21c4a9599cdefb1ea18df
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-11 13:06:09 -0400

    Configure vi focused inputrc

--- flake.nix
+++ flake.nix
@@ -176,6 +176,17 @@
               forwardAgent = true;
               includes = [ "config.backup" ];
             };
+
+            home.file.".inputrc".text = ''
+              set show-all-if-ambiguous on
+              set completion-ignore-case on
+              set mark-directories on
+              set mark-symlinked-directories on
+              set match-hidden-files off
+              set visible-stats on
+              set keymap vi
+              set editing-mode vi-insert
+            '';
           };
         }
       ];
