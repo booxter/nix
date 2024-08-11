9f4d09f90141216f45ff2667952861d114eed259
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-10 20:39:57 -0400

    Install coreutils for system

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      # environment.systemPackages = [ pkgs.git ];
+      environment.systemPackages = [ pkgs.coreutils ];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
