a367fb40c8a8e5318fa80808b8dcadf98820d3a1
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-11 22:17:19 -0400

    git config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -7,6 +7,28 @@
     package = pkgs.gitAndTools.gitFull;
     userEmail = "ihar.hrachyshka@gmail.com";
     userName = "Ihar Hrachyshka";
+    ignores = [
+      "*.swp"
+    ];
+
+    extraConfig = {
+      pw = {
+	server = "https://patchwork.ozlabs.org/api/1.2";
+	project = "ovn";
+      };
+      sendemail = {
+	confirm = "auto";
+	smtpServer = "smtp.gmail.com";
+	smtpServerPort = 587;
+	smtpEncryption = "tls";
+	smtpUser = "ihrachys@redhat.com";
+      };
+      rerere.enabled = true;
+      branch.sort = "-committerdate";
+    };
+
+    diff-so-fancy.enable = true;
+    diff-so-fancy.markEmptyLines = false;
   };
   programs.gh.enable = true;
 
@@ -24,7 +46,7 @@
 
   home.packages = with pkgs; [
     tig
-    gitAndTools.gitFull
+    git-pw
     telegram-desktop
     raycast
   ];
