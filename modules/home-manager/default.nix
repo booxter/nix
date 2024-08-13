1f1ccb307219806463927e24788a49fe14302e2a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-13 13:37:05 -0400

    Add alacritty binding to open new window

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -141,9 +141,20 @@
 
   programs.alacritty = {
     enable = true;
-    settings.font = {
-      normal.family = "MesloLGS Nerd Font Mono";
-      size = 16;
+    settings = {
+      font = {
+        normal.family = "MesloLGS Nerd Font Mono";
+        size = 16;
+      };
+      keyboard = {
+        bindings = [
+          {
+            key = "Return";
+            mods = "Command";
+            action = "CreateNewWindow";
+          }
+        ];
+      };
     };
   };
 
