01dd646e31935f258679510705222e26cef9d077
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-18 19:44:59 -0400

    Sync new files in git-sync-repo module

--- modules/home-manager/modules/git-sync-repo.nix
+++ modules/home-manager/modules/git-sync-repo.nix
@@ -1,4 +1,4 @@
 { lib, pkgs, gh-repo, destdir, ... }: lib.hm.dag.entryAfter ["writeBoundary"] ''
   GIT_SSH_COMMAND=${pkgs.openssh}/bin/ssh ${pkgs.git}/bin/git clone git@github.com:${gh-repo}.git ${destdir} || true
-  pushd ${destdir} && ${pkgs.git}/bin/git config --bool branch.master.sync true && popd
+  pushd ${destdir} && ${pkgs.git}/bin/git config --bool branch.master.sync true && ${pkgs.git}/bin/git config --bool branch.master.syncNewFiles true && popd
   ''
