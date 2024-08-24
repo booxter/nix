567c4afc590d8cb39bb0f1b9f76888d091cc0d8e
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-24 16:39:08 -0400

    kitty: initial config

--- modules/home-manager/programs/kitty.nix
+++ modules/home-manager/programs/kitty.nix
@@ -1,8 +1,17 @@
 {
   enable = true;
+  theme = "Gruvbox Dark Hard";
   shellIntegration.enableZshIntegration = true;
   font = {
     name = "MesloLGS Nerd Font Mono";
     size = 16;
+  };
+  settings = {
+    macos_quit_when_last_window_closed = true;
+    close_on_child_death = true;
+    enable_audio_bell = false;
+    mouse_hide_wait = 0;
+    strip_trailing_spaces = "always";
+    scrollback_lines = 100000;
   };
 }
