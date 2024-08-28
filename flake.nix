d0ae3d1de6e7921f03e7aa9819141798c9fbe737
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-08-28 18:32:54 -0400

    style

--- flake.nix
+++ flake.nix
@@ -4,18 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs_kitty_fix.url = "github:booxter/nixpkgs/fix-kitty-and-darwin-tests";
+
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     # home-manager.url = "github:nix-community/home-manager";
     # Use a fork with a fix for thunderbird and firefox profiles.ini Version=2
     # See: https://github.com/nix-community/home-manager/pull/5724
     # TODO: overlay just for the packages of interest
     home-manager.url = "github:booxter/home-manager/fix-thunderbird-aarch64";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
     nur.url = "github:nix-community/NUR";
     nur.inputs.nixpkgs.follows = "nixpkgs";
+
     emacs.url = "github:nix-community/emacs-overlay";
     emacs.inputs.nixpkgs.follows = "nixpkgs";
   };
