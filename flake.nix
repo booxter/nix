d20bd2036e0b845b211c74f9bbfe533aa6d5696f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-09-01 19:08:09 -0400

    Switch back to master nix-darwin

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs_kitty_fix.url = "github:leiserfg/nixpkgs/fix-kitty-nerfont";
 
-    # nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.url = "github:booxter/nix-darwin/suppress-gpg-connect-agent-stderr";
+    nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     # home-manager.url = "github:nix-community/home-manager";
