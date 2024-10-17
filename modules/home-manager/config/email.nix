8e1238e8a372b65e1b2ce782b86dcf46e0d19748
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-10-17 18:04:37 -0400

    refactor: introduce common email config

--- modules/home-manager/config/email.nix
+++ modules/home-manager/config/email.nix
@@ -1,28 +1,24 @@
-{ pkgs, ...}: {
-  default = {
-    primary = true;
+{ pkgs, ...}: let
+  commonCfg = {
     realName = "Ihar Hrachyshka";
     flavor = "gmail.com";
-    address = "ihar.hrachyshka@gmail.com";
-    userName = "ihar.hrachyshka@gmail.com";
-    passwordCommand = "${pkgs.pass}/bin/pass show priv/google.com-mutt";
     imap.host = "imap.gmail.com";
     smtp.host = "smtp.gmail.com";
     thunderbird.enable = true;
     mbsync.enable = true;
     notmuch.enable = true;
   };
+in {
+  default = {
+    primary = true;
+    address = "ihar.hrachyshka@gmail.com";
+    userName = "ihar.hrachyshka@gmail.com";
+    passwordCommand = "${pkgs.pass}/bin/pass show priv/google.com-mutt";
+  } // commonCfg;
   work = {
-    realName = "Ihar Hrachyshka";
-    flavor = "gmail.com";
     address = "ihrachys@redhat.com";
     aliases = [ "ihar@redhat.com" ];
     userName = "ihrachys@redhat.com";
     passwordCommand = "${pkgs.pass}/bin/pass show rh/google.com-app-password-macpro";
-    imap.host = "imap.gmail.com";
-    smtp.host = "smtp.gmail.com";
-    thunderbird.enable = true;
-    mbsync.enable = true;
-    notmuch.enable = true;
-  };
+  } // commonCfg;
 }
