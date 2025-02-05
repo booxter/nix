485683443fce8dffe9f4ce3d53f81ad6468e1749
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-02-05 09:10:26 -0500

    Patch ollama to not crash on pull
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,20 @@
     inherit (import inputs.nixpkgs-mailsend-go { inherit (prev) system; }) mailsend-go;
     inherit (import inputs.nixpkgs-cb_thunderlink-native { inherit (prev) system; }) cb_thunderlink-native;
     flox = inputs.flox.packages.${prev.system}.default;
+    ollama = let
+      pkgs = import inputs.nixpkgs { inherit (prev) system; };
+    in pkgs.ollama.overrideAttrs (oldAttrs: {
+      patches = [
+        (pkgs.fetchpatch2 {
+          url = "https://github.com/ollama/ollama/pull/8746/commits/fb801e1e1f4de9d295ae306278fb1040ad42ffde.patch?full_index=1";
+          hash = "sha256-HstYCxtd2vlqIlffGxra82wUlFH7F98YFQs7CXdQ26Q=";
+        })
+        (pkgs.fetchpatch2 {
+          url = "https://github.com/ollama/ollama/pull/8746/commits/dac0154f21c576e8dfc729f699ff18baa37181c4.patch?full_index=1";
+          hash = "sha256-QqluubLyr5kxMJk9wnKP0PZHE733oq3fA/0gSGe78ak=";
+        })
+      ];
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
