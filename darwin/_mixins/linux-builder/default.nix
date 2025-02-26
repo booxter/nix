fb05aab1738e6d94775e1ae547b82ef218b32db7
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-02-25 23:06:57 -0500

    linux-builder: configure to 127.0.0.1 instead of localhost
    macos is insane and fails to resolve localhost for ssh.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- darwin/_mixins/linux-builder/default.nix
+++ darwin/_mixins/linux-builder/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ lib, ... }: {
   nix = {
     linux-builder = {
       # if custom config is ever broken to the point the machine cannot start
@@ -22,4 +22,17 @@
       trusted-users = [ "@admin" ];
     };
   };
+
+  # Collect logs for debugging purposes.
+  launchd.daemons.linux-builder = { serviceConfig = { StandardOutPath = "/var/log/darwin-builder.log"; StandardErrorPath = "/var/log/darwin-builder.log"; }; };
+
+  # For unclear reasons, macos sometimes fails to resolve localhost for ssh... 127.0.0.1 seems to work.
+  environment.etc."ssh/ssh_config.d/100-linux-builder.conf".text = lib.mkBefore ''
+      Host linux-builder
+        User builder
+        Hostname 127.0.0.1
+        HostKeyAlias linux-builder
+        Port 31022
+        IdentityFile /etc/nix/builder_ed25519
+    '';
 }
