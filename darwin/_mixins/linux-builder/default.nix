050307caaddbd0a1e91ddaae8209c05be9b0136b
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-02-26 20:17:10 -0500

    pull latest nix-darwin to fix localhost resolution
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- darwin/_mixins/linux-builder/default.nix
+++ darwin/_mixins/linux-builder/default.nix
@@ -25,14 +25,4 @@
 
   # Collect logs for debugging purposes.
   launchd.daemons.linux-builder = { serviceConfig = { StandardOutPath = "/var/log/darwin-builder.log"; StandardErrorPath = "/var/log/darwin-builder.log"; }; };
-
-  # For unclear reasons, macos sometimes fails to resolve localhost for ssh... 127.0.0.1 seems to work.
-  environment.etc."ssh/ssh_config.d/100-linux-builder.conf".text = lib.mkBefore ''
-      Host linux-builder
-        User builder
-        Hostname 127.0.0.1
-        HostKeyAlias linux-builder
-        Port 31022
-        IdentityFile /etc/nix/builder_ed25519
-    '';
 }
