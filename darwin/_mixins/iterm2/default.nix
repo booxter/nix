174a6de45ea061cd734560c92eca27cac5e0b4ca
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-03-02 20:55:15 -0500

    create a separate mixin for kinit-pass service
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- darwin/_mixins/iterm2/default.nix
+++ darwin/_mixins/iterm2/default.nix
@@ -1,33 +1,7 @@
-{ pkgs, username, ...} : {
+{ ... } : {
   # TODO: use launchd.user.agents.iterm2.serviceConfig instead?
   environment.userLaunchAgents.iterm2 = {
     source = ./iterm2-login.plist;
     target = "iterm2.plist";
-  };
-  environment.userLaunchAgents.kinit-pass = {
-    text = ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-          <key>Label</key>
-          <string>kinit-pass</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>${pkgs.kinit-pass}</string>
-          </array>
-          <key>EnvironmentVariables</key>
-          <dict>
-              <key>PASSWORD_STORE_DIR</key>
-              <string>/Users/${username}/.local/share/password-store</string>
-          </dict>
-          <key>StartInterval</key>
-          <integer>${toString (60 * 60 * 8)}</integer>
-          <key>RunAtLoad</key>
-          <true/>
-      </dict>
-      </plist>
-    '';
-    target = "kinit-pass.plist";
   };
 }
