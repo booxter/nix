0f8d1e160d9042a8c73ce0fd2cf148990550acd6
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-03-02 20:52:20 -0500

    kinit on vpn connected
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- pkgs/vpn/default.nix
+++ pkgs/vpn/default.nix
@@ -1,12 +1,18 @@
-# TODO: how to kinit-pass on VPN up?
 { pkgs, ... }: pkgs.writeScriptBin "vpn" ''
+  # Why the hell it's seventh?.. Is there a way to refer to it by name?..
   osascript << EOF
     tell application "Viscosity"
-    if the state of the first connection is "Connected" then
-      disconnect "Red Hat Global VPN"
-    else
+    if the state of the seventh connection is not "Connected" then
       connect "Red Hat Global VPN"
     end if
     end tell
   EOF
+
+  # Wait for connection to be established
+  while [ "$(osascript -e 'tell application "Viscosity" to state of the seventh connection')" != "Connected" ]; do
+    echo 'waiting for VPN connection to be established...'
+    sleep 1
+  done
+
+  ${pkgs.kinit-pass}/bin/kinit-pass
   ''
