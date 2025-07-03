17867c874350df03a45258b70eef62744ea4d076
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2025-07-02 20:52:30 -0400

    register authorized key for mlt machine for darwin

--- darwin/default.nix
+++ darwin/default.nix
@@ -62,6 +62,7 @@
   services.openssh.enable = true;
   users.users.${username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILF2Ga7NLRUkAqv6B4GDya40U1mQalWo8XOhEhOPF3zW ihrachyshka@Mac.lan"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHt25mSiJLQjx2JECMuhTZEV6rlrOYk3CT2cUEdXAoYs ihrachyshka@ihrachyshka-mlt"
   ];
 
   # Create /etc/zshrc that loads the nix-darwin environment.
