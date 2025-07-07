fe3edd24d2be3ed49d71fa1054975b05c3ef9137
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-07-06 20:15:24 -0400

    scripts: don't restart vm if nothing changed

--- scripts/update-nixos-vbox.sh
+++ scripts/update-nixos-vbox.sh
@@ -9,8 +9,20 @@ VMI_DESTFILE=$VMI_DESTDIR/$VM.ova
 echo "Updating NixOS VirtualBox image..."
 make linux-vbox
 
+echo "Checking if md5sum changed for $VM..."
+NEW_MD5=$(md5sum ./result/$VM*.ova | awk '{print $1}')
+if [ -e $VMI_DESTFILE -a -e $VMI_DESTFILE.md5sum ]; then
+	OLD_MD5=$(cat $VMI_DESTFILE.md5sum)
+
+	if [ "$NEW_MD5" == "$OLD_MD5" ]; then
+			echo "No changes detected in VM image. Do nothing."
+			exit 0
+	fi
+fi
+
 echo "Copying NixOS VirtualBox image to $VMI_DESTDIR..."
 rsync --progress $(realpath ./result/$VM*) $VMI_DESTFILE
+echo $NEW_MD5 > $VMI_DESTFILE.md5sum
 
 echo "Importing NixOS VirtualBox image..."
 ./scripts/start-nixos-vbox-import.sh
