0327dd6ccb212295d7bab14746b65b97a0d1ca7e
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-07-10 21:19:24 -0400

    fix community builder creds - use separate key

--- darwin/_mixins/community-builders/default.nix
+++ darwin/_mixins/community-builders/default.nix
@@ -13,22 +13,29 @@
     };
     extraConfig = let
       identityFile = "/Users/${username}/.ssh/id_ed25519";
+      communityBuilderIdentityFile = "/Users/${username}/.ssh/nix-community-builders";
       user = "booxter";
     in ''
       Host darwin-builder
         Hostname darwin-build-box.nix-community.org
-        IdentityFile ${identityFile}
+        IdentityFile ${communityBuilderIdentityFile}
         User ${user}
 
-      Host linux-builder
+      Host remote-linux-builder
         Hostname aarch64-build-box.nix-community.org
-        IdentityFile ${identityFile}
+        IdentityFile ${communityBuilderIdentityFile}
         User ${user}
 
-      Host linux-x86-builder
+      Host remote-linux-x86-builder
         Hostname build-box.nix-community.org
-        IdentityFile ${identityFile}
+        IdentityFile ${communityBuilderIdentityFile}
         User ${user}
+
+      # TODO: move elsewhere since it's not a community builder
+      Host int-linux-builder
+        Hostname builder
+        IdentityFile ${identityFile}
+        User ihrachyshka
     '';
   };
   environment.systemPackages = [ pkgs.openssh ];
