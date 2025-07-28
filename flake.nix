d7064ccd5acedca8f1377572fa11df96a4809da3
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-07-28 01:27:01 +0000

    enable lan dns

--- flake.nix
+++ flake.nix
@@ -252,26 +252,54 @@
             };
 
             environment.systemPackages = with pkgs; [
+              dig
+              git
               lm_sensors
             ];
 
             # TODO: enable ipv6, dns
+            # TODO: use secret management for internal info?
             services.dnsmasq = {
               enable = true;
+              resolveLocalQueries = true;
               settings = {
                 interface = "end0";
                 dhcp-authoritative = true;
                 dhcp-rapid-commit = true;
+
                 dhcp-range = [ "192.168.10.1,192.168.20.255" ];
 
                 listen-address = ["192.168.1.1"];
 
                 dhcp-option = [
                   "option:router,192.168.0.1"
-                  "option:dns-server,192.168.0.1"
+                  "option:dns-server,192.168.1.1"
                 ];
 
-                domain = "booxter.lan";
+                server = [
+                  "192.168.0.1"
+                ];
+
+                domain-needed = true;
+
+                host-record = [
+                  "egress,192.168.0.1"
+                  "dhcp,192.168.1.1"
+                ];
+
+                # TODO: parametrize, eg.: https://github.com/kradalby/dotfiles/blob/6bae60204e1caab84262b2b1b7be013eeec80547/machines/dev.ldn/dnsmasq.nix
+                dhcp-host = [
+                  # infra
+                  "7c:b7:7b:04:05:99,mdx,192.168.10.100" # MDX-8
+
+                  # clients (wifi)
+                  "76:90:da:3c:46:db,mmini,192.168.11.1"
+                  "36:95:f3:6f:a7:f7,ihrachyshka-mlt,192.168.11.2"
+
+                  # lab
+                  "78:2d:7e:24:2d:f9,sw.lab,192.168.15.1" # switch
+                  "78:72:64:43:9c:3f,nas.lab,192.168.15.2" # asustor
+                ];
 
                 enable-tftp = true;
                 tftp-root = "/var/lib/dnsmasq/tftp";
@@ -283,6 +311,7 @@
               };
             };
             networking.firewall.allowedUDPPorts = [
+              53 # DNS
               67 # DHCP
               69 # TFTP
             ];
