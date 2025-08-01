9a76235105439c9ac3875023e69d305c80594321
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2025-08-01 14:14:43 -0400

    configure hostname for all configs

--- common/default.nix
+++ common/default.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, hostname, ... }:
 {
   imports = [
     ./_mixins/nix
     ./_mixins/ssh
     ./_mixins/terminfo
   ];
+
+  networking.hostName = hostname;
 
   environment.systemPackages = with pkgs; [
     git
--- nixos/pi5/default.nix
+++ nixos/pi5/default.nix
@@ -2,7 +2,6 @@
   netIface = "end0";
 in {
   networking = {
-    hostName = hostname;
     interfaces.end0 = {
       ipv4.addresses = [{
         address = "192.168.1.1";
