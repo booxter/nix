28ceb772cc09d85ea33a65e88f80d20de13b8b82
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2025-08-05 11:39:14 -0400

    make fetch-config accept remote host

--- scripts/fetch-config.sh
+++ scripts/fetch-config.sh
@@ -1,6 +1,15 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p git
 
+set -euo pipefail
+
+if [ "$#" -eq 1 ]; then
+	REMOTE_HOST="$1"
+	scp "$0" "$REMOTE_HOST:~/fetch-config.sh"
+	ssh "$REMOTE_HOST" ./fetch-config.sh
+	exit 0
+fi
+
 mkdir -p ~/src
 pushd ~/src
 git clone https://github.com/booxter/nix.git
