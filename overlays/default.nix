de2b704ec80b245042141530320b77f5055c10d5
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-08-15 20:01:41 -0400

    pull moltenvk with a fix for metal llama server

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,35 +19,39 @@
 
     # https://github.com/NixOS/nixpkgs/pull/417062
     inherit (import inputs.nixpkgs-krunkit { inherit (prev) system; }) libkrun-efi;
-    krunkit = ((import inputs.nixpkgs-krunkit { inherit (prev) system; }).krunkit.override {
-      libkrun-efi = (_final.libkrun-efi.override {
-        withGpu = true;
-      }).overrideAttrs (oldAttrs: rec {
-        version = "1.14.0";
-        src = _final.fetchFromGitHub {
-          owner = "containers";
-          repo = "libkrun";
-          tag = "v${version}";
-          hash = "sha256-tXF1AkcwSBj+e3qEGR/NqB1U+y4+MIRbaL9xB0cZQbQ=";
-        };
-        cargoDeps = _final.rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-IrJVP7I8NDB4KyZ0g8D6Tx+dT+lN8Yg8uRT9tXlL/8s=";
-        };
-      });
-    }).overrideAttrs (oldAttrs: rec {
-      version = "0.2.2";
-      src = _final.fetchFromGitHub {
-        owner = "containers";
-        repo = "krunkit";
-        tag = "v${version}";
-        hash = "sha256-fyk3vF/d+qv347XI1+z7zzd5JxRRjopnKIV6GATA3Ac=";
-      };
-      cargoDeps = _final.rustPlatform.fetchCargoVendor {
-        inherit src;
-        hash = "sha256-4WLmIlk2OSmIt9FPDjCPHD5JyBszCWMwVEhbnnKKNQY=";
-      };
-    });
+    krunkit =
+      ((import inputs.nixpkgs-krunkit { inherit (prev) system; }).krunkit.override {
+        libkrun-efi =
+          (_final.libkrun-efi.override {
+            withGpu = true;
+          }).overrideAttrs
+            (oldAttrs: rec {
+              version = "1.14.0";
+              src = _final.fetchFromGitHub {
+                owner = "containers";
+                repo = "libkrun";
+                tag = "v${version}";
+                hash = "sha256-tXF1AkcwSBj+e3qEGR/NqB1U+y4+MIRbaL9xB0cZQbQ=";
+              };
+              cargoDeps = _final.rustPlatform.fetchCargoVendor {
+                inherit src;
+                hash = "sha256-IrJVP7I8NDB4KyZ0g8D6Tx+dT+lN8Yg8uRT9tXlL/8s=";
+              };
+            });
+      }).overrideAttrs
+        (oldAttrs: rec {
+          version = "0.2.2";
+          src = _final.fetchFromGitHub {
+            owner = "containers";
+            repo = "krunkit";
+            tag = "v${version}";
+            hash = "sha256-fyk3vF/d+qv347XI1+z7zzd5JxRRjopnKIV6GATA3Ac=";
+          };
+          cargoDeps = _final.rustPlatform.fetchCargoVendor {
+            inherit src;
+            hash = "sha256-4WLmIlk2OSmIt9FPDjCPHD5JyBszCWMwVEhbnnKKNQY=";
+          };
+        });
 
     podman = prev.podman.override {
       extraPackages = [
@@ -55,26 +59,50 @@
       ];
     };
 
-    ramalama = ((import inputs.nixpkgs-master { inherit (prev) system; }).ramalama.override {
-      podman = _final.podman;
-    }).overrideAttrs (oldAttrs: rec {
-      version = "0.12.0";
+    ramalama =
+      let
+        moltenvk = ((import inputs.nixpkgs { inherit (prev) system; }).moltenvk).overrideAttrs (oldAttrs: {
+          # https://github.com/KhronosGroup/MoltenVK/pull/2441
+          patches = oldAttrs.patches ++ [
+            (_final.fetchpatch {
+              url = "https://github.com/KhronosGroup/MoltenVK/pull/2441/commits/70a3a152168bb8aefd1553e57ca165a590b77a06.patch";
+              hash = "sha256-7yyq/6H0/4m0zlu2f9XKkxKGISCW4suQLRxcLlmyWXQ=";
+            })
+            ./0001-MVKShaderLibrary-Handle-specializtion-with-macros.patch
+          ];
+        });
 
-      src = _final.fetchFromGitHub {
-        owner = "containers";
-        repo = "ramalama";
-        tag = "v${version}";
-        hash = "sha256-Hozyf0yfB0XhxWeA3SS24BPfDDXYa2AXY8/gLh8ZFcU=";
-      };
+        vulkan-loader = (import inputs.nixpkgs { inherit (prev) system; }).vulkan-loader.override {
+          inherit moltenvk;
+        };
+        llama-cpp = (import inputs.nixpkgs { inherit (prev) system; }).llama-cpp.override {
+          inherit vulkan-loader;
+        };
+        llama-cpp-vulkan = (import inputs.nixpkgs { inherit (prev) system; }).llama-cpp-vulkan.override {
+          inherit llama-cpp;
+        };
 
-      postInstall = let
-        lib = _final.lib;
-      in ''
-        wrapProgram $out/bin/ramalama \
-        --prefix PATH : ${lib.makeBinPath [ _final.podman _final.llama-cpp ]}
-      '';
-
-    });
+      in
+      ((import inputs.nixpkgs-master { inherit (prev) system; }).ramalama.override {
+        podman = _final.podman;
+      }).overrideAttrs
+        (oldAttrs: {
+          postInstall =
+            let
+              lib = _final.lib;
+            in
+            ''
+              wrapProgram $out/bin/ramalama \
+              --prefix PATH : ${
+                lib.makeBinPath [
+                  _final.podman
+                  llama-cpp-vulkan
+                  _final.python313Packages.mlx-lm
+                  _final.python313Packages.huggingface-hub
+                ]
+              }
+            '';
+        });
 
     # python312 = prev.python312.override {
     #   packageOverrides = final: prev: {
