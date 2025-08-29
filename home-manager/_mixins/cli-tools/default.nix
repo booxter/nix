c77dc4a8c90458afde1e2aec76ac6c5bb573f22c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-08-29 19:18:27 -0400

    Revert "include air-sdk in my python env"
    This reverts commit 652b782c69fff20f96b29428ed912776b81daaf9.

--- home-manager/_mixins/cli-tools/default.nix
+++ home-manager/_mixins/cli-tools/default.nix
@@ -124,13 +124,9 @@
     zstd
 
     # python
-    (python313Full.withPackages (
-      ps: with ps; [
-        air-sdk
-      ]
-    ))
-    python313Packages.ipython
-    python313Packages.tox
+    python312Full
+    python312Packages.ipython
+    python312Packages.tox
   ];
 
   home.sessionVariables = {
