2077ec13b0087b528e775c86dc8f83e5362edc63
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-09-01 16:00:06 -0400

    sketchybar: indicate where stock trends to

--- home-manager/_mixins/aerospace/sketchybar/plugins/stock.sh
+++ home-manager/_mixins/aerospace/sketchybar/plugins/stock.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-price=$(curl https://groww.in/us-stocks/nvda | grep -oP '"lastPrice":\K[0-9.]*')
+data=$(curl -s https://groww.in/us-stocks/nvda)
+
+lastPrice=$(echo $data | grep -oP '"lastPrice":\K[0-9.]*')
+openingPrice=$(echo $data | grep -oP '"openingPrice":\K[0-9.]*')
+
+if (( $(echo "$lastPrice < $openingPrice" | bc -l) )); then
+	COLOR="0xffff0000"
+	ICON="􀁩"
+else
+	COLOR="0xffa6e3a1"
+	ICON="􀁧"
+fi
 
 sketchybar --set $NAME \
-	icon=􀖘  \
-	label="${price}"
+	icon=$ICON  \
+	icon.color=$COLOR \
+	label="${lastPrice}"
