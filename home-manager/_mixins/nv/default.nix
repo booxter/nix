a249e1c07d0c98cc9b5a7ec1a4980c95bdb4cae1
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2025-09-12 11:23:27 -0400

    nix fmt

--- home-manager/_mixins/nv/default.nix
+++ home-manager/_mixins/nv/default.nix
@@ -12,15 +12,19 @@
     "$HOME/.krew/bin"
   ];
 
-  home.packages = with pkgs; ([
-    gitlab-ci-local
-  ]
-  ++ lib.optionals isDesktop [
-    jinjanator
-    kind
-    kubectl
-    kubernetes-helm
-    slack
-    vault-bin
-  ]);
+  home.packages =
+    with pkgs;
+    (
+      [
+        gitlab-ci-local
+      ]
+      ++ lib.optionals isDesktop [
+        jinjanator
+        kind
+        kubectl
+        kubernetes-helm
+        slack
+        vault-bin
+      ]
+    );
 }
