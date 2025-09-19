0cb60e0c226d78fbedcc3160936df09fabdbd1d3
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2025-09-19 10:06:17 -0400

    nixvim: activate dap ui

--- home-manager/_mixins/nixvim/default.nix
+++ home-manager/_mixins/nixvim/default.nix
@@ -133,5 +133,22 @@
       " \vl to run latest command
       map <Leader>vl :VimuxRunLastCommand<CR>
     '';
+
+    extraConfigLua = ''
+      # Automatically open DAP UI on debugger activated
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    '';
   };
 }
