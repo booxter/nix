8ef49b9343452dfb09a50d0809ff837ac1a8e3c9
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-09-28 02:24:42 +0000

    dark mode for gtk

--- home-manager/_mixins/hyprland/default.nix
+++ home-manager/_mixins/hyprland/default.nix
@@ -7,6 +7,41 @@ in
   home.packages = with pkgs; [
     wl-clipboard
   ];
+
+  # TODO: rename module?
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  home.sessionVariables.GTK_THEME = "palenight";
+
   wayland.windowManager.hyprland = {
     enable = isLinux;
     xwayland.enable = true;
