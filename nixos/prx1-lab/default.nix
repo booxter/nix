2b0daa615804dca732d8ff5dd4e236559072f7d9
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-10-19 18:41:41 -0400

    Revert "Pass amdgpu through via vfio"
    Turns out iGPUs are not vfio passthru compatible...
    
    This reverts commit 6a3bc7eca4646c771e198da502f255f6a89754c8.

--- nixos/prx1-lab/default.nix
+++ nixos/prx1-lab/default.nix
@@ -9,19 +9,4 @@
 
   # Intel CPU microcode updates
   hardware.cpu.intel.updateMicrocode = true;
-
-  # Configure AMD GPU for passthru
-  boot.initrd.kernelModules = [
-    "vfio_pci"
-    "vfio"
-    "vfio_iommu_type1"
-   ];
-
-  boot.kernelParams = [
-    "amd_iommu=on"
-    "iommu=pt"
-    "vfio-pci.ids=1002:13c0"
-  ];
-
-  boot.blacklistedKernelModules = [ "amdgpu" ];
 }
