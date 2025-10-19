9a3463d4ea721639d3aef7a3e4ec6c8eec3f4c00
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-10-19 14:36:38 -0400

    Enable firmware on prx nodes

--- nixos/prx1-lab/default.nix
+++ nixos/prx1-lab/default.nix
@@ -3,4 +3,10 @@
   imports = [
     (import ../../disko { })
   ];
+
+  services.fwupd.enable = true;
+  hardware.enableRedistributableFirmware = true;
+
+  # Intel CPU microcode updates
+  hardware.cpu.intel.updateMicrocode = true;
 }
