0fed19f810df0b35ddd1467e631966bc196b0a65
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-10-19 23:47:48 -0400

    configure cachevm as substituter

--- common/default.nix
+++ common/default.nix
@@ -58,6 +58,17 @@
   };
 
   programs.zsh.enable = true;
+
+  nix = {
+    settings = {
+      extra-substituters = [
+        "http://prox-cachevm:8080/default/"
+      ];
+      extra-trusted-public-keys = [
+        "default:+epFjzN1YKGqqeraQczdEfRyIuzgWd6/nrifa0467QQ="
+      ];
+    };
+  };
 }
 // lib.optionalAttrs (!isWork) {
   # TODO: move elsewhere
