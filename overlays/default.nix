bb69df0e60f75529bae6cabd356fa05094875a19
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-12-29 20:27:06 -0500

    Adopt jellarr with newest jf version

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,7 +16,6 @@
 
       pkgs = getPkgs inputs.nixpkgs;
       pkgsLldb = getPkgs inputs.debugserver;
-      pkgsJF = getPkgs inputs.jellyfin-pinned;
     in
     {
       # https://github.com/NixOS/nixpkgs/pull/374846
@@ -24,18 +23,6 @@
 
       # pull latest from nixpkgs; ignore what comes from rpi5 repo nixpkgs
       inherit (pkgs) netbootxyz-efi;
-
-      # pin to older version while declarative module fixes it for newer
-      inherit (pkgsJF) jellyfin-web;
-      jellyfin = pkgsJF.jellyfin.overrideAttrs (old: {
-        # fix tmdbid from file name ignored
-        patches = old.patches or [ ] ++ [
-          (pkgs.fetchpatch {
-            url = "https://github.com/jellyfin/jellyfin/pull/14955/commits/95d057d2ac0ef5f673dd3a7765a741703521c4a6.patch";
-            hash = "sha256-rEacvK/E+EDgHmOS09Di2F5CAA/f6eRby1mez+jHiQI=";
-          })
-        ];
-      });
 
       whisparr = prev.whisparr.overrideAttrs (old: rec {
         pname = "whisparr";
