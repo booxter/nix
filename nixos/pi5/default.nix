2ddec70f5a4ec24656170fae44d75b664294962d
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-01-10 17:10:49 -0500

    Revert "Run syslogd -f on dhcp node"
    Served its need.
    
    This reverts commit 3883bdaceed3375d05e59951e94ef14e2a78b105.

--- nixos/pi5/default.nix
+++ nixos/pi5/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, hostname, ... }:
 let
   mainIface = "end0";
   guestIface = "wlan0";
@@ -126,15 +126,8 @@ in
     53 # DNS
     67 # DHCP
     69 # TFTP
-    514 # syslog
   ];
   systemd.tmpfiles.rules = [
     "L+ /var/lib/dnsmasq/tftp/netboot.xyz.efi - - - - ${pkgs.netbootxyz-efi}"
   ];
-
-  # syslogd -r for remote debugging
-  services.syslogd = {
-    enable = true;
-    enableNetworkInput = true;
-  };
 }
