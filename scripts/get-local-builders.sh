6f07ddeabbca4752bb45c1becc1473b2090c20f8
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-02-14 19:59:31 -0500

    add tests for scripts/get-local-builders.sh

--- scripts/get-local-builders.sh
+++ scripts/get-local-builders.sh
@@ -16,13 +16,15 @@ elif [[ "${1:-}" == "--local" ]]; then
 fi
 
 builders=""
+nix_conf="${NIX_CONF:-/etc/nix/nix.conf}"
+nix_machines="${NIX_MACHINES:-/etc/nix/machines}"
 
-if [[ -r /etc/nix/nix.conf ]]; then
-  builders="$(awk -F= '/^builders[[:space:]]*=/ {gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2); print $2}' /etc/nix/nix.conf | tail -n 1)"
+if [[ -r "$nix_conf" ]]; then
+  builders="$(awk -F= '/^[[:space:]]*builders[[:space:]]*=/ {gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2); print $2}' "$nix_conf" | tail -n 1)"
 fi
 
-if [[ -z "$builders" && -r /etc/nix/machines ]]; then
-  builders="$(awk 'NF && $1 !~ /^#/' /etc/nix/machines | paste -sd ';' -)"
+if [[ -z "$builders" && -r "$nix_machines" ]]; then
+  builders="$(awk 'NF && $1 !~ /^#/' "$nix_machines" | paste -sd ';' -)"
 fi
 
 if [[ -z "$builders" ]]; then
