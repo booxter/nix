c0c6dfcee58fcd754931a473a87c570428d8a0f5
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-03-04 18:52:39 -0500

    Split DNS

--- nixos/pi5/default.nix
+++ nixos/pi5/default.nix
@@ -79,6 +79,8 @@ in
       host-record = [
         "egress,${gwAddr}"
         "dhcp,${mainAddr}"
+        # Split DNS: keep internal clients on LAN path for external service domains.
+        "jf.ihar.dev,au.ihar.dev,js.ihar.dev,192.168.16.3"
       ];
 
       # TODO: parametrize, eg.: https://github.com/kradalby/dotfiles/blob/6bae60204e1caab84262b2b1b7be013eeec80547/machines/dev.ldn/dnsmasq.nix
