6d63878a4977c5093e2796384994ecaab1a9b9ff
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-03-12 21:43:32 -0700

    Accept xcode license on activation

--- darwin/_mixins/homebrew/default.nix
+++ darwin/_mixins/homebrew/default.nix
@@ -1,5 +1,19 @@
-{ username, ... }:
+{ lib, username, ... }:
 {
+  system.activationScripts.preActivation.text = lib.mkBefore ''
+    if [ -x /usr/bin/xcodebuild ] && [ -x /usr/bin/xcode-select ]; then
+      dev_dir="$(/usr/bin/xcode-select -p 2>/dev/null || true)"
+      case "$dev_dir" in
+        */Xcode.app/Contents/Developer)
+          if ! /usr/bin/xcodebuild -license check >/dev/null 2>&1; then
+            echo "Accepting Xcode license..."
+            /usr/bin/xcodebuild -license accept
+          fi
+          ;;
+      esac
+    fi
+  '';
+
   homebrew = {
     enable = true;
     onActivation.autoUpdate = true;
