45337a91e4f4db68db6eff61396e30fbd7514a78
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-03-21 18:05:14 -0400

    Don't reboot frame machine on kernel upgrade

--- nixos/frame/default.nix
+++ nixos/frame/default.nix
@@ -1,10 +1,13 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     (import ../../disko/luks.nix { })
     inputs.nixos-hardware.nixosModules.framework-desktop-amd-ai-max-300-series
     ./ups.nix
   ];
+
+  # This host needs manual unlock after boot; never auto-reboot on upgrades.
+  system.autoUpgrade.allowReboot = lib.mkForce false;
 
   networking.wireless.enable = false;
   networking.wireless.secretsFile = "/etc/wireless.secrets";
