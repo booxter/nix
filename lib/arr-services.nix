62bc2e95f0d049d10be4fa90bf6dc48b2ed8906f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-03-27 20:56:59 -0400

    blackbox: use final urls instead of redirects

--- lib/arr-services.nix
+++ lib/arr-services.nix
@@ -6,49 +6,56 @@
   srvarrPorts,
 }:
 let
-  mkExternal = id: title: url: icon: {
+  mkExternal = id: title: url: icon: probeUrl: {
     inherit
       icon
       id
+      probeUrl
       title
       url
       ;
-    probeUrl = url;
     scope = "external";
   };
 
-  mkInternal = id: title: port: icon: {
+  mkInternal = id: title: port: icon: probePath: {
     inherit
       icon
       id
       title
       ;
-    probeUrl = "http://${srvarrProbeHost}:${toString port}/";
+    probeUrl = "http://${srvarrProbeHost}:${toString port}${probePath}";
     scope = "internal";
     url = "http://${srvarrDisplayHost}:${toString port}/";
   };
 in
 [
-  (mkExternal "jellyfin" "Jellyfin" "https://jf.ihar.dev" "sh:jellyfin")
-  (mkExternal "jellyseerr" "Jellyseerr" "https://js.ihar.dev" "sh:jellyseerr")
+  (mkExternal "jellyfin" "Jellyfin" "https://jf.ihar.dev" "sh:jellyfin" "https://jf.ihar.dev/web/")
+  (mkExternal "jellyseerr" "Jellyseerr" "https://js.ihar.dev" "sh:jellyseerr"
+    "https://js.ihar.dev/login"
+  )
   {
     icon = "sh:grafana";
     id = "grafana";
-    probeUrl = grafanaProbeUrl;
+    probeUrl = "${grafanaProbeUrl}login";
     scope = "internal";
     title = "Grafana";
     url = grafanaDisplayUrl;
   }
-  (mkInternal "radarr" "Radarr" srvarrPorts.radarr "sh:radarr")
-  (mkInternal "sonarr" "Sonarr" srvarrPorts.sonarr "sh:sonarr")
-  (mkInternal "lidarr" "Lidarr" srvarrPorts.lidarr "sh:lidarr")
-  (mkExternal "audiobookshelf" "Audiobookshelf" "https://au.ihar.dev" "sh:audiobookshelf")
-  (mkInternal "readarr" "Readarr" srvarrPorts.readarr "sh:readarr")
-  (mkInternal "readarr-audio" "Readarr Audio" srvarrPorts.readarrAudio "sh:readarr")
-  (mkInternal "bazarr" "Bazarr" srvarrPorts.bazarr "sh:bazarr")
-  (mkInternal "prowlarr" "Prowlarr" srvarrPorts.prowlarr "sh:prowlarr")
-  (mkInternal "transmission" "Transmission" srvarrPorts.transmission "sh:transmission")
+  (mkInternal "radarr" "Radarr" srvarrPorts.radarr "sh:radarr" "/login")
+  (mkInternal "sonarr" "Sonarr" srvarrPorts.sonarr "sh:sonarr" "/login")
+  (mkInternal "lidarr" "Lidarr" srvarrPorts.lidarr "sh:lidarr" "/")
+  (mkExternal "audiobookshelf" "Audiobookshelf" "https://au.ihar.dev" "sh:audiobookshelf"
+    "https://au.ihar.dev"
+  )
+  (mkInternal "readarr" "Readarr" srvarrPorts.readarr "sh:readarr" "/login")
+  (mkInternal "readarr-audio" "Readarr Audio" srvarrPorts.readarrAudio "sh:readarr" "/login")
+  (mkInternal "bazarr" "Bazarr" srvarrPorts.bazarr "sh:bazarr" "/")
+  (mkInternal "prowlarr" "Prowlarr" srvarrPorts.prowlarr "sh:prowlarr" "/login")
+  (mkInternal "transmission" "Transmission" srvarrPorts.transmission "sh:transmission"
+    "/transmission/web/"
+  )
   (mkInternal "sabnzbd" "SABNZB" srvarrPorts.sabnzbd
     "https://raw.githubusercontent.com/sabnzbd/sabnzbd/70d5134d28a0c1cddff49c97fa013cb67c356f9e/icons/logo-arrow.svg"
+    "/login/"
   )
 ]
