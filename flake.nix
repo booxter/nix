98648ff6305f5ae5f90ef35aa36dbf53dbd25071
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-04-08 11:38:03 -0500

    Untie nixpkgs for llm-agents from main input
    Should help with cache reuse.

--- flake.nix
+++ flake.nix
@@ -65,7 +65,6 @@
     nixarr.inputs.nixpkgs.follows = "nixpkgs";
 
     llm-agents.url = "github:numtide/llm-agents.nix";
-    llm-agents.inputs.nixpkgs.follows = "nixpkgs";
 
     tig = {
       url = "github:jonas/tig";
