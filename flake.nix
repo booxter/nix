4f467a09d0f4d5c30c5db9017c3f1b2b6cd6fbc7
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-04-13 18:27:21 -0400

    Drop jellyfin version override

--- flake.nix
+++ flake.nix
@@ -9,9 +9,6 @@
     # Keep Transmission pinned independently from the moving release-25.11 branch.
     # TODO: remove this input when trackers allow 4.1.0+.
     nixpkgs-transmission.url = "github:NixOS/nixpkgs/12d60a4f2d5f2cc96e93ae5615328245d49ac2e8";
-    # Temporary Jellyfin 10.11.8 backport until it lands in the pinned nixpkgs.
-    # TODO: remove this input once the nixpkgs release branch includes Jellyfin 10.11.8+.
-    nixpkgs-jellyfin.url = "github:whispersofthedawn/nixpkgs/670522ebae401381a5fe85c5e419697cf62a811e";
     # Temporary diff-so-fancy 1.4.10 backport until it lands in nixpkgs-unstable.
     # TODO: remove this input once nixpkgs includes diff-so-fancy 1.4.10+.
     nixpkgs-diff-so-fancy.url = "github:booxter/nixpkgs/e523d5636f2edfa5688d5fa05b3adc64ef6d9a89";
--- overlays/default.nix
+++ overlays/default.nix
@@ -18,7 +18,6 @@
       pkgsLldb = getPkgs inputs.debugserver;
       pkgsRelease = getPkgs inputs.nixpkgs-25_11;
       pkgsTransmission = getPkgs inputs.nixpkgs-transmission;
-      pkgsJellyfin = getPkgs inputs.nixpkgs-jellyfin;
       pkgsDiffSoFancy = getPkgs inputs.nixpkgs-diff-so-fancy;
       pkgsFirefoxUnwrapped = getPkgs inputs.nixpkgs-firefox-unwrapped;
       pkgsThunderbirdUnwrapped = getPkgs inputs.nixpkgs-thunderbird-unwrapped;
@@ -39,15 +38,9 @@
           throw "Failed to extract version from ${packagePath}"
         else
           builtins.head match;
-      releaseJellyfinVersion = getPackageVersion inputs.nixpkgs-25_11 "/pkgs/by-name/je/jellyfin/package.nix";
       nixpkgsDiffSoFancyVersion = getPackageVersion inputs.nixpkgs "/pkgs/by-name/di/diff-so-fancy/package.nix";
     in
-    if prev.lib.versionAtLeast releaseJellyfinVersion "10.11.8" then
-      throw ''
-        Temporary nixpkgs-jellyfin override is stale: nixpkgs-25_11 already provides jellyfin ${releaseJellyfinVersion}.
-        Remove the nixpkgs-jellyfin input and the corresponding jellyfin/jellyfin-web overlay entries.
-      ''
-    else if prev.lib.versionAtLeast nixpkgsDiffSoFancyVersion "1.4.10" then
+    if prev.lib.versionAtLeast nixpkgsDiffSoFancyVersion "1.4.10" then
       throw ''
         Temporary nixpkgs-diff-so-fancy override is stale: nixpkgs already provides diff-so-fancy ${nixpkgsDiffSoFancyVersion}.
         Remove the nixpkgs-diff-so-fancy input and the corresponding diff-so-fancy overlay entry.
@@ -68,10 +61,7 @@
         transmission_4 = pinnedTransmission;
         transmission = pinnedTransmission;
 
-        # Carry the upstream Jellyfin 10.11.8 backport from nixpkgs PR #507426
-        # until it lands in the pinned nixpkgs input.
-        inherit (pkgsJellyfin) jellyfin-web;
-        jellyfin = pkgsJellyfin.jellyfin.overrideAttrs (old: {
+        jellyfin = prev.jellyfin.overrideAttrs (old: {
           patches = old.patches or [ ] ++ [
             # Catch websocket keepalive send races.
             # Upstream: https://github.com/jellyfin/jellyfin/issues/14837
