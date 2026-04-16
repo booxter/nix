78a48cf280f8715c4a73abce72dc2fe17b2efbfb
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-04-16 18:14:12 -0400

    vikunja: enable mailer

--- nixos/orgvm/default.nix
+++ nixos/orgvm/default.nix
@@ -5,8 +5,23 @@ let
   vikunjaTimezone = "America/New_York";
 in
 {
+  sops.defaultSopsFile = ../../secrets/prox-orgvm.yaml;
+
+  sops.secrets.vikunjaMailerPassword = {
+    key = "vikunja/mailer/password";
+    restartUnits = [ "vikunja.service" ];
+  };
+
+  sops.templates."vikunja-mailer.env" = {
+    content = ''
+      VIKUNJA_MAILER_PASSWORD=${config.sops.placeholder.vikunjaMailerPassword}
+    '';
+    restartUnits = [ "vikunja.service" ];
+  };
+
   services.vikunja = {
     enable = true;
+    environmentFiles = [ config.sops.templates."vikunja-mailer.env".path ];
     frontendScheme = "http";
     frontendHostname = "${config.services.avahi.hostName}.local:${toString vikunjaPort}";
     port = vikunjaPort;
@@ -16,6 +31,13 @@ in
         week_start = 1;
       };
       metrics.enabled = true;
+      mailer = {
+        enabled = true;
+        host = "smtp.gmail.com";
+        port = 587;
+        username = "ihar.hrachyshka@gmail.com";
+        fromemail = "ihar.hrachyshka@gmail.com";
+      };
       service = {
         timezone = vikunjaTimezone;
         enableregistration = false;
