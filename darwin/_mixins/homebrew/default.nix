775aed315e7438e5145d1f4bcfcdd7b7be555e0d
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-04-26 14:48:55 -0400

    Remove todoist and chargpt desktop app

--- darwin/_mixins/homebrew/default.nix
+++ darwin/_mixins/homebrew/default.nix
@@ -18,13 +18,11 @@
     enable = true;
     onActivation.autoUpdate = true;
     casks = [
-      "chatgpt"
       "docker-desktop"
       "element"
       "openvpn-connect"
       "sf-symbols"
       "spotify" # spotify can't keep its shit together with hashes
-      "todoist-app"
       "wireshark-chmodbpf"
     ];
   };
