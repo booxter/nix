2d6ab92cc2bb46f5ec0d03c3ce7f8a0b9c199650
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-15 20:28:27 -0400

    Fix jellyfin session detection

--- overlays/default.nix
+++ overlays/default.nix
@@ -83,15 +83,10 @@
             url = "https://github.com/booxter/jellyfin/commit/c64abc489e.patch";
             hash = "sha256-/Y2QiBkeLY4Wi+RlgFcNuzLPuwOF1sRyf7hnBuUEzAM=";
           })
-          # Ignore stale playback callbacks after the original session is gone.
+          # Ignore stale progress updates unless transcode job exists.
           (prev.fetchpatch {
-            url = "https://github.com/booxter/jellyfin/commit/fa992be8f438d7cce1d8d8d3a0b534e7309e6dc7.patch";
-            hash = "sha256-Vq0oFhaLwt6Oweq+xnIsIg3vJPgQb7G32MBDg0IPPUo=";
-          })
-          # Clear orphaned transcode progress instead of flipping it to direct play.
-          (prev.fetchpatch {
-            url = "https://github.com/booxter/jellyfin/commit/070f0919d1130c3ce5729f87700e2b6b23483bf0.patch";
-            hash = "sha256-4UXJ2Ut9yXEL5JXpNVqaUw++X7iC7terL/WywA5MQXQ=";
+            url = "https://github.com/booxter/jellyfin/commit/3b63ec92420305d24e0fe90a452f0cdcbb624872.patch";
+            hash = "sha256-X5qv8+R2s/zk411gQHyNhRaf9VRFSG+47W8Fy0N+96U=";
           })
         ];
       });
