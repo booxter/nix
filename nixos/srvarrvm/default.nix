c94c7d571abeb2574b3222cbf2aaab44dafa0c59
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-21 23:20:22 -0400

    Use inventory directly for srvarr service links

--- nixos/srvarrvm/default.nix
+++ nixos/srvarrvm/default.nix
@@ -10,21 +10,33 @@
 let
   srvarrSpec = hostInventory.nixosHostSpecsByName.srvarr;
   beastNfsAddress = hostInventory.dhcpReservationsByHostname.beast.ip;
-  serviceCatalog = import ../../lib/services.nix {
-    inherit hostInventory;
-    srvarrPorts = {
-      aurral = config.systemd.services.aurral.environment.PORT;
-      audiobookshelf = config.nixarr.audiobookshelf.port;
-      bazarr = config.nixarr.bazarr.port;
-      lidarr = config.nixarr.lidarr.port;
-      prowlarr = config.nixarr.prowlarr.port;
-      radarr = config.nixarr.radarr.port;
-      sabnzbd = config.nixarr.sabnzbd.guiPort;
-      shelfmark = config.nixarr.shelfmark.port;
-      sonarr = config.nixarr.sonarr.port;
-      transmission = config.nixarr.transmission.uiPort;
-    };
+  srvarrPorts = {
+    aurral = config.systemd.services.aurral.environment.PORT;
+    audiobookshelf = config.nixarr.audiobookshelf.port;
+    bazarr = config.nixarr.bazarr.port;
+    lidarr = config.nixarr.lidarr.port;
+    prowlarr = config.nixarr.prowlarr.port;
+    radarr = config.nixarr.radarr.port;
+    sabnzbd = config.nixarr.sabnzbd.guiPort;
+    shelfmark = config.nixarr.shelfmark.port;
+    sonarr = config.nixarr.sonarr.port;
+    transmission = config.nixarr.transmission.uiPort;
   };
+  serviceCatalog = map (
+    service:
+    if service.scope == "external" then
+      service
+    else if service.owner == "fana" then
+      service
+      // {
+        url = "http://${service.displayHost}:3000/";
+      }
+    else
+      service
+      // {
+        url = "http://${service.displayHost}:${toString srvarrPorts.${service.id}}/";
+      }
+  ) hostInventory.services;
   mediaPath = "/data/media";
   # Resilient NFS client behavior:
   # - hard: block I/O until the server is back (avoid soft I/O errors).
