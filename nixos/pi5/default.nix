db0abedd4ac66b0b9d50697cf611bd079c0c6f6d
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-21 22:45:38 -0400

    Use DHCP range helper in pi5

--- nixos/pi5/default.nix
+++ nixos/pi5/default.nix
@@ -2,40 +2,7 @@
 let
   lan = hostInventory.site.lan;
   beastAddress = hostInventory.dhcpReservationsByHostname.beast.ip;
-  ipToInt =
-    ip:
-    lib.foldl' (acc: octet: acc * 256 + octet) 0 (map lib.toInt (lib.splitString "." ip));
-  pow2 = n: if n == 0 then 1 else 2 * pow2 (n - 1);
-  cidrToRange =
-    cidr:
-    let
-      parts = lib.splitString "/" cidr;
-      address = builtins.elemAt parts 0;
-      prefixLength = lib.toInt (builtins.elemAt parts 1);
-      networkSize = pow2 (32 - prefixLength);
-      start = builtins.div (ipToInt address) networkSize * networkSize;
-    in
-    {
-      inherit start;
-      end = start + networkSize - 1;
-    };
-  rangeToIntBounds = range: {
-    start = ipToInt range.start;
-    end = ipToInt range.end;
-  };
-  rangeOverlapsCidr =
-    range: cidr:
-    let
-      rangeBounds = rangeToIntBounds range;
-      cidrBounds = cidrToRange cidr;
-    in
-    rangeBounds.start <= cidrBounds.end && cidrBounds.start <= rangeBounds.end;
-  mkDhcpRangeExclusionAssertions =
-    name: dhcpRange:
-    map (excludeRange: {
-      assertion = builtins.all (range: !(rangeOverlapsCidr range excludeRange)) dhcpRange.ranges;
-      message = "LAN DHCP pool `${name}` must not overlap excluded subnet `${excludeRange}`.";
-    }) dhcpRange.excludeRanges;
+  dhcpRanges = import ../../lib/dhcp-ranges.nix { inherit lib; };
   renderDhcpReservation =
     reservation:
     builtins.concatStringsSep "," (
@@ -64,8 +31,8 @@ in
   ];
 
   assertions =
-    mkDhcpRangeExclusionAssertions "main" lan.dhcpRanges.main
-    ++ mkDhcpRangeExclusionAssertions "guest" lan.dhcpRanges.guest;
+    dhcpRanges.mkExclusionAssertions "main" lan.dhcpRanges.main
+    ++ dhcpRanges.mkExclusionAssertions "guest" lan.dhcpRanges.guest;
 
   networking = {
     interfaces.end0 = {
