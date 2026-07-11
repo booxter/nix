22b16d168d95c23c65a53d50bff667f30540514f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-07-11 10:41:12 -0400

    git: set upstream on first push

--- home-manager/_mixins/scm/default.nix
+++ home-manager/_mixins/scm/default.nix
@@ -105,9 +105,13 @@ in
         pruneTags = true;
       };
 
-      # Make explicit force-with-lease pushes reject remote commits that have
-      # not first been integrated locally.
-      push.useForceIfIncludes = true;
+      push = {
+        autoSetupRemote = true;
+
+        # Make explicit force-with-lease pushes reject remote commits that have
+        # not first been integrated locally.
+        useForceIfIncludes = true;
+      };
 
       url = {
         # Keep GitHub pushes on SSH instead of gh's broad HTTPS OAuth token,
