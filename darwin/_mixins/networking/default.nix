bd33dbe93cbb3722362fd07223fb8a179340f27b
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-07-11 12:44:38 -0400

    darwin: resolve LAN domain through gateway

--- darwin/_mixins/networking/default.nix
+++ darwin/_mixins/networking/default.nix
@@ -1,10 +1,18 @@
 {
+  hostInventory,
   hostname,
   isWork,
   lib,
   ...
 }:
+let
+  lan = hostInventory.site.lan;
+in
 {
+  environment.etc."resolver/${lan.domain}".text = ''
+    nameserver ${lan.gateway.address}
+  '';
+
   # Can't configure networking on managed work devices
   networking = lib.optionalAttrs (!isWork) {
     knownNetworkServices =
