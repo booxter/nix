aac03c4392218123869440b24e97dd72f2dac073
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-05 13:58:39 -0400

    tests: remove cache NixOS test

--- nixos-tests.nix
+++ nixos-tests.nix
@@ -5,7 +5,6 @@ helpers.forAllSystems (
     pkgs = import inputs.nixpkgs { inherit system; };
   in
   pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-    cache = import ./tests/nixos/cache.nix { inherit pkgs; };
     oauth2-proxy-gate = import ./tests/nixos/oauth2-proxy-gate.nix { inherit pkgs; };
   }
 )
