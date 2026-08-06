fb77bf662ef48bc328bd7c6fb81b25c63733534c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-06 19:38:04 -0400

    attic: derive client policy from realms

--- inv/realms.nix
+++ inv/realms.nix
@@ -2,12 +2,18 @@
 {
   home = {
     secretDomain = "main";
-    services.observability = {
-      loki = {
-        writeUrl = "https://loki.${lanDomain}/loki/api/v1/push";
-        mtls = true;
+    services = {
+      attic = {
+        cacheName = "local";
+        endpoint = "https://nix-cache.${lanDomain}";
       };
-      nodeExporter.mtls = true;
+      observability = {
+        loki = {
+          writeUrl = "https://loki.${lanDomain}/loki/api/v1/push";
+          mtls = true;
+        };
+        nodeExporter.mtls = true;
+      };
     };
   };
 
