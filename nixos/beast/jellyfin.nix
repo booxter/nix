b0f8a0bb73c83214c7159dfe5d860fedce43e0b1
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-10 15:55:22 -0400

    beast: split Jellyfin configuration

---
+++ nixos/beast/jellyfin.nix
@@ -0,0 +1,68 @@
+{
+  host.jellyfin = {
+    enable = true;
+    media = {
+      provider = "beast";
+      resource = "media";
+    };
+    meilisearch.enable = true;
+    libraries = {
+      movies = {
+        name = "Movies";
+        path = "movies";
+        kind = "movies";
+      };
+      shows = {
+        name = "Shows";
+        path = "shows";
+        kind = "series";
+      };
+      family = {
+        name = "Family";
+        path = "family";
+        kind = "movies";
+      };
+      anime = {
+        name = "Anime";
+        path = "anime";
+        kind = "movies";
+      };
+      docu = {
+        name = "Docu";
+        path = "docu";
+        kind = "movies";
+      };
+      stand-up = {
+        name = "Stand-up";
+        path = "standup";
+        kind = "movies";
+      };
+      attic = {
+        name = "Attic";
+        path = "attic";
+        kind = "movies";
+        audience = "adult";
+        metadataPolicy = "tmdb-first";
+      };
+      fruit = {
+        name = "Fruit";
+        path = "xxx";
+        kind = "movies";
+        audience = "adult";
+        metadataPolicy = "tmdb-first";
+      };
+      fruitsies = {
+        name = "Fruitsies";
+        path = "fruitsies";
+        kind = "series";
+        audience = "adult";
+      };
+      music = {
+        name = "Music";
+        path = "music";
+        kind = "music";
+      };
+    };
+    backups.stagingDirectory = "/volume2/backups/staging/jellyfin";
+  };
+}
