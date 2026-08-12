b39feaa0d4a538af14cbb9ca73d394b26669156e
booxter <90200+booxter@users.noreply.github.com>
2026-08-12 08:01:52 +0000

    packages: update nico-cli

--- hm/_mixins/nv/pkgs/nico-cli/default.nix
+++ hm/_mixins/nv/pkgs/nico-cli/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "nico-cli";
-  version = "2.1.0-rc.2";
+  version = "2.1.0-rc.3";
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "infra-controller";
     rev = "v${version}";
-    hash = "sha256-zrRPVPWxykyA9SeDz6z/3ZJ8X0eCq4vvjn3+kuLjB2c=";
+    hash = "sha256-8Fu9uqKOWRcAO1C72HHZ/cyCX9z/oVvtAk6qFpYoRa8=";
   };
 
   modRoot = "./rest-api";
