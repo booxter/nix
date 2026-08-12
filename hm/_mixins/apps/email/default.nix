9125356a35cf5a4d1ddf6a9bd863830df517c08f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-11 21:58:20 -0400

    home-manager: expose Thunderbird enable option

--- hm/_mixins/apps/email/default.nix
+++ hm/_mixins/apps/email/default.nix
@@ -1,6 +1,13 @@
+{ osConfig, ... }:
+let
+  appsCfg = osConfig.host.userEnvironment.features.apps;
+in
 {
   imports = [
     ./gmailctl.nix
     ./thunderbird.nix
   ];
+
+  host.hm.thunderbird.enable =
+    appsCfg.enable && appsCfg.email.enable && appsCfg.email.thunderbird.enable;
 }
--- hm/_mixins/apps/email/thunderbird.nix
+++ hm/_mixins/apps/email/thunderbird.nix
@@ -8,8 +8,7 @@
 let
   inherit (osConfig.host) isDarwin;
   userEnvironment = osConfig.host.userEnvironment;
-  appsCfg = userEnvironment.features.apps;
-  emailCfg = appsCfg.email;
+  emailCfg = userEnvironment.features.apps.email;
   emailAccount = userEnvironment.emailAccounts.${emailCfg.account};
   identity = userEnvironment.identities.${emailAccount.identity};
   smtpTransport = userEnvironment.smtpTransports.${emailAccount.smtpTransport};
@@ -19,65 +18,69 @@ let
   };
   thunderbirdProfilesPath = if isDarwin then "Library/Thunderbird/Profiles" else ".thunderbird";
 in
-lib.mkIf (appsCfg.enable && emailCfg.enable) {
-  programs.thunderbird = lib.mkIf emailCfg.thunderbird.enable {
-    enable = true;
-    package = pkgs.thunderbird;
-    profiles.default = {
-      isDefault = true;
-      settings = {
-        # Sort by date in descending order using threaded view
-        "mailnews.default_sort_type" = 18;
-        "mailnews.default_sort_order" = 2;
-        "mailnews.default_view_flags" = 1;
-        "mailnews.default_news_sort_type" = 18;
-        "mailnews.default_news_sort_order" = 2;
-        "mailnews.default_news_view_flags" = 1;
+{
+  options.host.hm.thunderbird.enable = lib.mkEnableOption "managed Thunderbird email client";
 
-        # Disable autoupdates
-        "app.update.auto" = false;
-        "app.update.staging.enabled" = false;
+  config = lib.mkIf config.host.hm.thunderbird.enable {
+    programs.thunderbird = {
+      enable = true;
+      package = pkgs.thunderbird;
+      profiles.default = {
+        isDefault = true;
+        settings = {
+          # Sort by date in descending order using threaded view
+          "mailnews.default_sort_type" = 18;
+          "mailnews.default_sort_order" = 2;
+          "mailnews.default_view_flags" = 1;
+          "mailnews.default_news_sort_type" = 18;
+          "mailnews.default_news_sort_order" = 2;
+          "mailnews.default_news_view_flags" = 1;
 
-        # Remove some ui bloat
-        "mailnews.start_page.enabled" = false;
-        "mail.uidensity" = 0;
-        "mail.threadpane.listview" = 1;
+          # Disable autoupdates
+          "app.update.auto" = false;
+          "app.update.staging.enabled" = false;
 
-        "mail.ui.folderpane.view" = 1;
-        "mail.folder.views.version" = 1;
+          # Remove some ui bloat
+          "mailnews.start_page.enabled" = false;
+          "mail.uidensity" = 0;
+          "mail.threadpane.listview" = 1;
 
-        # Check IMAP subfolder for new messages
-        "mail.check_all_imap_folders_for_new" = true;
-        "mail.server.default.check_all_folders_for_new" = true;
+          "mail.ui.folderpane.view" = 1;
+          "mail.folder.views.version" = 1;
 
-        # Use the system browser for OAuth flows.
-        "mailnews.oauth.useExternalBrowser" = true;
+          # Check IMAP subfolder for new messages
+          "mail.check_all_imap_folders_for_new" = true;
+          "mail.server.default.check_all_folders_for_new" = true;
 
-        # Default the compose window and send path to plain text.
-        "mail.default_send_format" = 1;
+          # Use the system browser for OAuth flows.
+          "mailnews.oauth.useExternalBrowser" = true;
+
+          # Default the compose window and send path to plain text.
+          "mail.default_send_format" = 1;
+        };
       };
     };
-  };
 
-  accounts.email.accounts.default = {
-    inherit (emailAccount) flavor;
-    address = identity.email;
-    realName = identity.fullName;
-    primary = true;
-    smtp.host = lib.mkForce smtpTransport.server;
-    thunderbird = {
-      enable = emailCfg.thunderbird.enable;
-      perIdentitySettings = id: {
-        # The account UI stores "Compose messages in HTML format" per identity.
-        "mail.identity.id_${id}.compose_html" = false;
-      };
-      settings = id: {
-        "mail.server.server_${id}.authMethod" = authenticationMethod.${emailAccount.imapAuthentication};
-        # Thunderbird treats this as a filesystem path during folder/filter
-        # validation; keep it absolute.
-        "mail.server.server_${id}.directory" =
-          "${config.home.homeDirectory}/${thunderbirdProfilesPath}/default/ImapMail/${id}";
-        "mail.smtpserver.smtp_${id}.authMethod" = authenticationMethod.${emailAccount.smtpAuthentication};
+    accounts.email.accounts.default = {
+      inherit (emailAccount) flavor;
+      address = identity.email;
+      realName = identity.fullName;
+      primary = true;
+      smtp.host = lib.mkForce smtpTransport.server;
+      thunderbird = {
+        enable = true;
+        perIdentitySettings = id: {
+          # The account UI stores "Compose messages in HTML format" per identity.
+          "mail.identity.id_${id}.compose_html" = false;
+        };
+        settings = id: {
+          "mail.server.server_${id}.authMethod" = authenticationMethod.${emailAccount.imapAuthentication};
+          # Thunderbird treats this as a filesystem path during folder/filter
+          # validation; keep it absolute.
+          "mail.server.server_${id}.directory" =
+            "${config.home.homeDirectory}/${thunderbirdProfilesPath}/default/ImapMail/${id}";
+          "mail.smtpserver.smtp_${id}.authMethod" = authenticationMethod.${emailAccount.smtpAuthentication};
+        };
       };
     };
   };
