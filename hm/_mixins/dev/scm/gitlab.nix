d7ef4da1964b25190cde87ade8ce1d92beca0214
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-12 00:37:19 -0400

    home-manager: remove glab MR helper

--- hm/_mixins/dev/scm/gitlab.nix
+++ hm/_mixins/dev/scm/gitlab.nix
@@ -6,11 +6,7 @@
 }:
 let
   devCfg = osConfig.host.userEnvironment.features.dev;
-  scmPkgs = import ./pkgs { inherit pkgs; };
 in
 lib.mkIf (devCfg.enable && devCfg.scm.enable) {
-  home.packages = [
-    pkgs.glab
-    scmPkgs.glab-mr-create
-  ];
+  home.packages = [ pkgs.glab ];
 }
--- hm/_mixins/dev/scm/pkgs/default.nix
+++ hm/_mixins/dev/scm/pkgs/default.nix
@@ -3,6 +3,4 @@
   check-commit-message = pkgs.callPackage ./check-commit-message { };
 
   git-send-email-store-password = pkgs.callPackage ./git-send-email-store-password { };
-
-  glab-mr-create = pkgs.callPackage ./glab-mr-create { };
 }
