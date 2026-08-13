82772cca94dfb02e2d272db79903190109fc10cf
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-13 16:28:31 -0400

    lidarr: move deployment into host module

--- nixos/srvarr/lidarr.nix
+++ nixos/srvarr/lidarr.nix
@@ -1,13 +1,6 @@
 {
-  config,
-  lib,
-  ...
-}:
-let
-  mkServarrApp = import ./mk-servarr-app.nix { inherit config lib; };
-in
-mkServarrApp {
-  name = "lidarr";
-  apiGroup = "lidarr-api";
-  addUserToApiGroup = false;
+  host.lidarr = {
+    enable = true;
+    stateDir = "/data/.state/nixarr/lidarr";
+  };
 }
