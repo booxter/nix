3fa5f22171c8a98b3173955d2cdc770e489ffeb6
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-14 16:47:02 -0400

    mailer: consolidate realm policy module

--- nixos/_mixins/mailer/default.nix
+++ nixos/_mixins/mailer/default.nix
@@ -1,6 +1,39 @@
 {
-  imports = [
-    ./home.nix
-    ./options.nix
-  ];
+  config,
+  lib,
+  ...
+}:
+let
+  mailerType = lib.types.submodule {
+    options = {
+      relayHost = lib.mkOption {
+        type = lib.types.nonEmptyStr;
+        description = "SMTP relay hostname.";
+      };
+
+      relayPort = lib.mkOption {
+        type = lib.types.port;
+        description = "SMTP relay port.";
+      };
+
+      address = lib.mkOption {
+        type = lib.types.nonEmptyStr;
+        description = "SMTP account and sender address for realm services.";
+      };
+    };
+  };
+in
+{
+  options.host.mailer = lib.mkOption {
+    type = with lib.types; nullOr mailerType;
+    default = null;
+    internal = true;
+    description = "SMTP delivery policy for services in this host's realm.";
+  };
+
+  config.host.mailer = lib.mkIf (config.host.realm == "home") {
+    relayHost = "smtp.gmail.com";
+    relayPort = 587;
+    address = "ihar.hrachyshka@gmail.com";
+  };
 }
--- nixos/_mixins/mailer/home.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, ... }:
-lib.mkIf (config.host.realm == "home") {
-  host.mailer = {
-    relayHost = "smtp.gmail.com";
-    relayPort = 587;
-    address = "ihar.hrachyshka@gmail.com";
-  };
-}
