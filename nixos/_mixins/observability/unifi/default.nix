038db78ee0d35f67217e21ca28edc8072b5b5858
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-13 22:54:17 -0400

    observability: inline UniFi assertions

--- nixos/_mixins/observability/unifi/default.nix
+++ nixos/_mixins/observability/unifi/default.nix
@@ -1,9 +1,21 @@
-{ lib, ... }:
+{ config, lib, ... }:
+let
+  cfg = config.host.observability.unifi;
+  controller = config.host.site.lan.ipController;
+in
 {
-  imports = [
-    ./assertions.nix
-    ./service.nix
-  ];
+  imports = [ ./service.nix ];
 
   options.host.observability.unifi.enable = lib.mkEnableOption "UniFi network observability";
+
+  config.assertions = lib.optionals cfg.enable [
+    {
+      assertion = controller != null;
+      message = "host.observability.unifi requires an IP controller for this physical site";
+    }
+    {
+      assertion = controller != null && controller.flavor == "unifi";
+      message = "host.observability.unifi requires a UniFi site network controller";
+    }
+  ];
 }
