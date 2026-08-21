fe7bb6b34756a0c90087781cce1a3df6ef739f77
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-21 17:10:13 -0400

    backups: expose service composition

--- nixos/_mixins/backups/topology/default.nix
+++ nixos/_mixins/backups/topology/default.nix
@@ -11,21 +11,11 @@ let
     inherit backupConfigurations config lib;
   };
   inherit (topology) client server;
-
-  qosEnabled = backups.server != null && backups.server.offsite != null && backups.server.offsite.qos;
 in
 {
   config = lib.mkMerge [
     {
       _module.args.backupTopology = topology;
-
-      host.qos.interfaces.wan = lib.mkIf qosEnabled {
-        device = config.host.network.primaryInterface;
-        limits.cloud-backup = {
-          rateMbit = config.host.site.policies.backups.maxUploadMbit;
-          match.users = server.offloadUsers;
-        };
-      };
     }
 
     (import ./assertions.nix {
