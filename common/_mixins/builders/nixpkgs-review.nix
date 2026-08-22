caf2cb61675cbb3caa408ff1e8bc25bff7579a7f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-22 08:10:00 -0400

    builders: share Nix machine formatter

--- common/_mixins/builders/nixpkgs-review.nix
+++ common/_mixins/builders/nixpkgs-review.nix
@@ -1,24 +1,14 @@
 { config, lib, ... }:
 let
+  formatBuilder = import ../../_lib/format-nix-builder.nix { inherit lib; };
   builders =
     if config.host.nix.builderClient != null then
       lib.filterAttrs (_: builder: builtins.elem "nixpkgs" builder.uses) config.host.nix.builder-pool
     else
       { };
-  formatList = values: if values == [ ] then "-" else lib.concatStringsSep "," values;
-  formatBuilder =
-    name: builder:
-    lib.concatMapStringsSep " " toString [
-      "${builder.protocol}://${builder.sshUser}@${name}"
-      (formatList builder.systems)
-      builder.sshKey
-      builder.maxJobs
-      builder.speedFactor
-      (formatList builder.supportedFeatures)
-      "-"
-      "-"
-    ];
-  builderString = lib.concatStringsSep " ; " (lib.mapAttrsToList formatBuilder builders);
+  builderString = lib.concatStringsSep " ; " (
+    lib.mapAttrsToList (name: builder: formatBuilder (builder // { hostName = name; })) builders
+  );
 in
 {
   options.host.nix.nixpkgs-review.builders = lib.mkOption {
