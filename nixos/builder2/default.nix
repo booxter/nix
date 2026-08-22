62c1bb19f4faaf5d5547aea76be297b9b5791dc4
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-22 01:32:16 -0400

    builders: limit concurrent jobs

--- nixos/builder2/default.nix
+++ nixos/builder2/default.nix
@@ -1,5 +1,5 @@
 {
-  host.nix.builder = { };
+  host.nix.builder.maxJobs = 2;
   host.network.macAddress = "bc:24:11:dc:ea:2c";
   host.proxmox.guest = {
     cluster = "lab";
--- nixos/builder3/default.nix
+++ nixos/builder3/default.nix
@@ -1,5 +1,5 @@
 {
-  host.nix.builder = { };
+  host.nix.builder.maxJobs = 2;
   host.network.macAddress = "bc:24:11:2a:ee:d7";
   host.proxmox.guest = {
     cluster = "lab";
