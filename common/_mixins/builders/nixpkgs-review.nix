18e58420948ab5fac93522c76221e72270cba00c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-23 16:59:51 -0400

    builders: include local Linux builder in nr

--- common/_mixins/builders/nixpkgs-review.nix
+++ common/_mixins/builders/nixpkgs-review.nix
@@ -1,21 +1,36 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  options,
+  ...
+}:
 let
   formatBuilder = import ../../_lib/format-nix-builder.nix { inherit lib; };
-  builders =
+  poolBuilders =
     if config.host.nix.builderClient != null then
       lib.filterAttrs (_: builder: builtins.elem "nixpkgs" builder.uses) config.host.nix.builder-pool
     else
       { };
-  builderString = lib.concatStringsSep " ; " (
-    lib.mapAttrsToList (name: builder: formatBuilder (builder // { hostName = name; })) builders
-  );
+  builders =
+    lib.mapAttrsToList (name: builder: builder // { hostName = name; }) poolBuilders
+    ++ config.host.nix.nixpkgs-review.additional-builders;
+  builderString = lib.concatStringsSep " ; " (map formatBuilder builders);
 in
 {
-  options.host.nix.nixpkgs-review.builders = lib.mkOption {
-    type = lib.types.str;
-    default = builderString;
-    readOnly = true;
-    internal = true;
-    description = "Complete nixpkgs-review machines-file argument.";
+  options.host.nix.nixpkgs-review = {
+    additional-builders = lib.mkOption {
+      type = options.nix.buildMachines.type;
+      default = [ ];
+      internal = true;
+      description = "Builders managed outside the fleet builder pool.";
+    };
+
+    builders = lib.mkOption {
+      type = lib.types.str;
+      default = builderString;
+      readOnly = true;
+      internal = true;
+      description = "Complete nixpkgs-review machines-file argument.";
+    };
   };
 }
