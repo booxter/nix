47b2316b856cfb7921e900ca433134c1fefd6afb
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-22 23:13:26 -0400

    Skip broken darwin tests for mlx

--- overlays/default.nix
+++ overlays/default.nix
@@ -58,6 +58,11 @@
             pyarrow = pythonPrev.pyarrow.overrideAttrs {
               doInstallCheck = false;
             };
+
+            # https://github.com/NixOS/nixpkgs/pull/555598
+            mlx = pythonPrev.mlx.overrideAttrs {
+              doInstallCheck = false;
+            };
           }
         )
       ];
