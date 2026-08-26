94904f76bf160529046b0426d4b0c28139a52906
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-25 22:29:03 -0400

    darwin: verify internal CA trust during activation

--- darwin/_mixins/internal-pki/default.nix
+++ darwin/_mixins/internal-pki/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 let
@@ -11,13 +10,13 @@ in
   system.activationScripts.postActivation.text = lib.mkIf (authority != null) (
     lib.mkAfter ''
       cert_path=${lib.escapeShellArg "${authority.rootCaCertificate}"}
-      desired_sha256="$(${pkgs.openssl}/bin/openssl x509 -in "$cert_path" -noout -fingerprint -sha256 | /usr/bin/cut -d= -f2 | /usr/bin/tr -d ':')"
 
-      if /usr/bin/security find-certificate -a -Z /Library/Keychains/System.keychain 2>/dev/null | /usr/bin/grep -Fq "SHA-256 hash: $desired_sha256"; then
+      if /usr/bin/security verify-cert -q -L -c "$cert_path" -p basic; then
         echo "Internal PKI root CA already trusted in System keychain."
       else
         echo "Adding internal PKI root CA to System keychain."
         /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "$cert_path"
+        /usr/bin/security verify-cert -q -L -c "$cert_path" -p basic
       fi
     ''
   );
