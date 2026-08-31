73f4ecb2eda19f011b1c81b7b2c68263b7e6089f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-30 22:34:58 -0400

    hermes: upgrade to 0.20.6

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
     };
 
     hermes-agent = {
-      url = "github:NousResearch/hermes-agent/v2026.7.7.2";
+      url = "github:NousResearch/hermes-agent/v2026.8.27";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
