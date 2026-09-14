4ee6a7cda58beecbfa3cf1559c491177f598212a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-09-14 16:39:31 -0400

    srvarr: restore repair stabilization interval
    Use the controller's 15-minute default now that automatic imports
    have completed successfully. This gives Radarr time to finish its own
    import work before repair is allowed to mutate a case.

--- nixos/srvarr/radarr-repair.nix
+++ nixos/srvarr/radarr-repair.nix
@@ -7,8 +7,6 @@
   host.radarr.repair.controller = {
     enable = true;
     transmissionUrl = transmissionModel.rpcUrl;
-    # TODO: Restore the 15m default after automatic repair rollout is verified.
-    stabilization = "10s";
     apply = {
       enable = true;
       allowedActions = [
