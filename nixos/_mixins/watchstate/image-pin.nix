aa198eea0c5d069cf3530d3d73dbe4dcb884c4c6
booxter <90200+booxter@users.noreply.github.com>
2026-09-18 07:36:57 +0000

    watchstate: update container image

--- nixos/_mixins/watchstate/image-pin.nix
+++ nixos/_mixins/watchstate/image-pin.nix
@@ -1,8 +1,8 @@
 {
   changelog = "https://github.com/arabcoders/watchstate/releases/tag/{tag}";
-  digest = "sha256:810cc6dafda19b9a6abab3527ead1cd044962d157acf230491d169c5e2493bbb";
-  hash = "sha256-9IrtV4G9KmoPYXJ1Fwt+5lgsMeejZShbsQkXn7xQ6cg=";
+  digest = "sha256:fa4bab16446613c27fd118d5ff007202b5893c1cce7e3a793250972b7c0505df";
+  hash = "sha256-CSB6bSt9cMF6PwN7D90jnFvmvbEjhMayQG/Gizqti2A=";
   image = "ghcr.io/arabcoders/watchstate";
-  tag = "v1.10.5";
+  tag = "v1.10.6";
   tagRegex = "^v[0-9]+\\.[0-9]+\\.[0-9]+$";
 }
