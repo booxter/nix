e3c73e9b41a64bb26629ecc2faee026752f4d844
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-09-25 11:18:10 -0400

    shelfmark: disable cross-mount hardlinks

--- nixos/srvarr/shelfmark/default.nix
+++ nixos/srvarr/shelfmark/default.nix
@@ -35,5 +35,9 @@
   services.shelfmark.environment = {
     # Keep multi-file ebook releases as separate Audiobookshelf items.
     FILE_ORGANIZATION = "rename";
+    # The torrent and book directories are separate sandbox mounts.
+    HARDLINK_TORRENTS = "false";
+    # The torrent and audiobook directories are separate sandbox mounts.
+    HARDLINK_TORRENTS_AUDIOBOOK = "false";
   };
 }
